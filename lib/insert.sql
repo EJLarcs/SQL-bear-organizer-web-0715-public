bcfa8251d358b526b416b92de581266e38f568c1
EJLarcs <ejlarcombe@gmail.com>
2015-08-14 10:12:21 -0400

    sorting the bears

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+VALUES
+(1, "Mr. Chocolate", 5, "M", "brown", "lovely", 1),
+(2, "Rowdy", 12, "M", "red", "angry", 1 ),
+(3, "Tabitha", 13, "F", "black", "nice", 1),
+(4, "Sergeant Brown", 9, "M", "brown", "introvert", 1),
+(5, "Melissa", 8, "F", "polar-bear", "playful", 1),
+(6, "Grinch", 2, "M", "blonde", "anti-christmas", 1),
+(7, "Wendy", 3, "F", "brown", "quiet", 1),
+(8, NULL, 9, "M", "black", "scheming", 1);
